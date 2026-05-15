b10989a822461a9bae096b353de709bf441892a2
Alex <sthevanmc@proton.me>
2026-05-15 18:38:25 -0500

    Delete pokeapi-wrapper directory

--- pokeapi-wrapper/src/routes/battle.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const ctrl = require("../controllers/battle.controller");
-
-router.get("/compare/:pokemon1/:pokemon2", ctrl.comparePokemon);
-router.get("/recommend", ctrl.recommendAgainstType);
-
-module.exports = router;
